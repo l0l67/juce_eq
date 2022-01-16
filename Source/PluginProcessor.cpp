31343ec8aa55bb9355410c4fd913f1a179d38287
l0l67 <jonathann34555@gmail.com>
2022-01-16 13:16:17 +0100

    .

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -91,10 +91,19 @@ void EQAudioProcessor::changeProgramName (int index, const juce::String& newName
 }
 
 //==============================================================================
-void EQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+void EQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock) {
+  // Use this method as the place to do any pre-playback
+  // initialisation that you need..
+
+  juce::dsp::ProcessSpec spec;
+
+  spec.maximumBlockSize = samplesPerBlock;
+  spec.numChannels = 1;
+  spec.sampleRate = sampleRate;
+
+  leftChain.prepare(spec);
+  rightChain.prepare(spec);
+
 }
 
 void EQAudioProcessor::releaseResources()
@@ -129,33 +138,31 @@ bool EQAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 }
 #endif
 
-void EQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
-{
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
+void EQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages) {
+  juce::ScopedNoDenormals noDenormals;
+  auto totalNumInputChannels  = getTotalNumInputChannels();
+  auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+  // In case we have more outputs than inputs, this code clears any output
+  // channels that didn't contain input data, (because these aren't
+  // guaranteed to be empty - they may contain garbage).
+  // This is here to avoid people getting screaming feedback
+  // when they first compile a plugin, but obviously you don't need to keep
+  // this code if your algorithm always overwrites all the output channels.
+  for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+      buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+  juce::dsp::AudioBlock<float> block(buffer);
 
-        // ..do something to the data...
-    }
+  auto leftBlock = block.getSingleChannelBlock(0);
+  auto rightBlock = block.getSingleChannelBlock(1);
+
+  juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+  juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+
+  leftChain.process(leftContext);
+  rightChain.process(rightContext);
+
 }
 
 //==============================================================================
